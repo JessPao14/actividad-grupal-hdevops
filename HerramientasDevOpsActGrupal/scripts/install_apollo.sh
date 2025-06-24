b138abfcdbf3548c3ebce30fffaf2edd2888ba70
jsscsimba753 <jsscsimba753@gmail.com>
2025-06-23 19:36:54 -0500

    Eliminar todos los archivos

--- HerramientasDevOpsActGrupal/scripts/install_apollo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update -y
-apt install -y curl gnupg nginx nodejs npm git
-npm install -g n
-n latest
-mkdir -p /opt/apollo && cd /opt/apollo
-npx create-apollo-server@latest
-nohup node index.js &
